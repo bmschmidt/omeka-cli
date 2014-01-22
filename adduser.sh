c5fad43c27e982921a8078e635b3bf6b2cb29aca
Benjamin Schmidt <bmschmidt@gmail.com>
2014-01-22 00:17:27 -0500

    adding contributor role as default

--- adduser.sh
+++ adduser.sh
@@ -2,9 +2,9 @@
 
 email=$1
 table_prefix="omeka_"
-
+username=$(echo $email | sed "s/@.*//g"); 
 
 
 egrep "=|\[database\]" db.ini | egrep "database|host|username|password|dbname" | sed 's/\[database\]/\[mysql\]/;s/username/user/g;s/dbname/database/' > my.cnf
 
-mysql --defaults-file=my.cnf -e  "INSERT INTO ${table_prefix}users (username,name,email,active) VALUES ('$email','','$email',1);"
+mysql --defaults-file=my.cnf -e  "INSERT INTO ${table_prefix}users (username,name,email,active,role) VALUES ('$username','','$email',1,'contributor');"
